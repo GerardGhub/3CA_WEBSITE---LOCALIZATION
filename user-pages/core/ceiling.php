98608117f3b3633af4d05905f3ae9a7f137b2af0
GerardGhub <singian28@gmail.com>
2024-04-03 11:45:57 +0800

    fixed missing

--- user-pages/core/ceiling.php
+++ user-pages/core/ceiling.php
@@ -412,7 +412,7 @@ if (!isset($_SESSION['userType']) || $_SESSION['userType'] !== 'user') {
 
                                         <div class="productInfo">
                                             <div class="product-price">
-                                                <p id="unitPrice">₱ </p><?php echo number_format($row['unit_cost'], 2); ?>
+                                                <p id="unitPrice">₱  </p><?php echo number_format($row['unit_cost'], 2); ?>
                                             </div>
                                             <div class="product-option">
 
